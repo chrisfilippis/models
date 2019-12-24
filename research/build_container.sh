f223cbbe1c47e59b074920ad949e814059c7c978
Chris Filippis <chris.filippis@vodafone.com>
2019-12-24 19:51:19 +0200

    add gpu argument

--- research/build_container.sh
+++ research/build_container.sh
@@ -4,5 +4,5 @@ docker rm cfil_models
 
 docker build --tag=cfil_models:latest .
 
-# docker run -it --name cfil_models --gpus all cfil_models:latest bash
-docker run -it --name cfil_models cfil_models:latest bash
+docker run -it --name cfil_models --gpus all cfil_models:latest bash
+# docker run -it --name cfil_models cfil_models:latest bash
