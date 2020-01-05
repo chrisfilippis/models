86a2d120dd6e918faf9e063cb9f038b80c51130e
Chris Filippis <chris.filippis@vodafone.com>
2020-01-05 14:01:09 +0200

    debug build script

--- research/build_container.sh
+++ research/build_container.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+# Exit immediately if a command exits with a non-zero status.
+set -e
+
 docker stop cfil_models
 
 docker rm cfil_models
@@ -6,7 +10,9 @@ docker build --tag=cfil_models:latest .
 
 docker run -it --name cfil_models --gpus all cfil_models:latest bash
 
+echo "copying init model file"
 docker cp ../../deeplabv3_pascal_train_aug_2018_01_04.tar.gz cfil_models:/var/project/deeplab/datasets/tooth/init_models/deeplabv3_pascal_train_aug_2018_01_04.tar.gz
+echo "copying init model direcctory"
 docker cp ../../deeplabv3_pascal_train_aug cfil_models:/var/project/deeplab/datasets/tooth/init_models/deeplabv3_pascal_train_aug
 
 # docker run -it --name cfil_models cfil_models:latest bash
