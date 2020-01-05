6d8df38e21c49ab91fbdcf09b31580b789906029
Chris Filippis <chris.filippis@vodafone.com>
2020-01-05 14:25:16 +0200

    fixes

--- research/build_container.sh
+++ research/build_container.sh
@@ -8,13 +8,12 @@ docker rm cfil_models
 
 docker build --tag=cfil_models:latest .
 
-docker create --name cfil_models --gpus all cfil_models:latest
+docker run -d --name cfil_models --gpus all cfil_models:latest
 
 echo "copying init model file"
 docker cp ../../deeplabv3_pascal_train_aug_2018_01_04.tar.gz cfil_models:/var/project/deeplab/datasets/tooth/init_models/deeplabv3_pascal_train_aug_2018_01_04.tar.gz
 echo "copying init model direcctory"
 docker cp ../../deeplabv3_pascal_train_aug cfil_models:/var/project/deeplab/datasets/tooth/init_models/deeplabv3_pascal_train_aug
 
-docker start cfil_models
-
+docker exec -it cfil_models bash
 # docker run -it --name cfil_models cfil_models:latest bash
