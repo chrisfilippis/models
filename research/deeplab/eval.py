7268849cc5aef99c574710ba5f45f4e3ba2fddd2
Chris Filippis <chris.filippis@vodafone.com>
2020-01-05 14:54:22 +0200

    temp fix

--- research/deeplab/eval.py
+++ research/deeplab/eval.py
@@ -158,6 +158,10 @@ def main(unused_argv):
     # Define the evaluation metric.
     metric_map = {}
     num_classes = dataset.num_of_classes
+     indices = tf.squeeze(tf.where(tf.less_equal(
+        labels, dataset.num_classes - 1)), 1)
+    labels = tf.cast(tf.gather(labels, indices), tf.int32)
+    predictions = tf.gather(predictions, indices)
     metric_map['eval/%s_overall' % predictions_tag] = tf.metrics.mean_iou(
         labels=labels, predictions=predictions, num_classes=num_classes,
         weights=weights)
