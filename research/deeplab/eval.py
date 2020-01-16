d797c13a8ba50307247deb2dfc2b11ff3fe2bd68
Chris Filippis <chris.filippis@vodafone.com>
2020-01-17 00:07:15 +0200

    ..

--- research/deeplab/eval.py
+++ research/deeplab/eval.py
@@ -176,9 +176,8 @@ def main(unused_argv):
     for c in range(num_classes):
 
       with tf.Session() as sess:
-        print('xxxxxxxxxxxxx', len(one_hot_labels[:, c].eval()))
+        # print('xxxxxxxxxxxxx', len(one_hot_labels[:, c].eval()))
         print('pppppppppppppp', len(one_hot_predictions[:, c].eval()))
-      exit()
 
       predictions_tag_c = '%s_class_%d' % (predictions_tag, c)
       tp, tp_op = tf.metrics.true_positives(
